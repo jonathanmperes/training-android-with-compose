3f52eda3e7d0f377d09546369c667e4dd3c25cb5
jonathanmperes <emailmarceloperes@gmail.com>
2022-12-27 14:47:05 -0300

    multiple parameters added to the birthdayGreeting function

--- src/main/kotlin/CreateAndUseFunctionsInKotlin.kt
+++ src/main/kotlin/CreateAndUseFunctionsInKotlin.kt
@@ -1,12 +1,12 @@
 fun main() {
-    val greeting = birthdayGreeting(name = "Roger")
+    val greeting = birthdayGreeting(name = "Kurt", age = 8)
     println(greeting)
 
-    println(birthdayGreeting(name = "John"))
+    println(birthdayGreeting(name = "Phoebe", age = 5))
 }
 
-fun birthdayGreeting(name: String): String {
+fun birthdayGreeting(name: String, age: Int): String {
     val nameGreeting = "Happy Birthday, $name!"
-    val ageGreeting = "You are now 5 years old!"
+    val ageGreeting = "You are now $age years old!"
     return "$nameGreeting\n$ageGreeting"
 }
