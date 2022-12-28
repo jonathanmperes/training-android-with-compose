7187c52c015a28d61ddedffcc1831e71c92c3011
jonathanmperes <emailmarceloperes@gmail.com>
2022-12-28 18:39:06 -0300

    problem solved: print message

---
+++ src/main/kotlin/practiceProblemsKotlinBasics.kt
@@ -0,0 +1,14 @@
+fun main() {
+    printMessages()
+}
+
+fun printMessages() {
+    println(
+        """
+            Use the val keyword when the value doesn't change.
+            Use the var keyword when the value can change.
+            When you define a function, you define the parameters that can be passed to it.
+            When you call a function, you pass arguments for the parameters.
+        """.trimIndent()
+    )
+}
